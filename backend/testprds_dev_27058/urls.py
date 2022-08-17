f5d273c2977025b489380297a106d7221addf408
Crowdbotics <engineering@crowdbotics.com>
2022-08-17 18:42:16 +0000

    add package chainerboard

--- backend/testprds_dev_27058/urls.py
+++ backend/testprds_dev_27058/urls.py
@@ -35,9 +35,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "testprdssssseewtes"
-admin.site.site_title = "testprdssssseewtes Admin Portal"
-admin.site.index_title = "testprdssssseewtes Admin"
+admin.site.site_header = "Testprds"
+admin.site.site_title = "Testprds Admin Portal"
+admin.site.index_title = "Testprds Admin"
 
 # swagger
 api_info = openapi.Info(
